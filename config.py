80d0ec0aca358a54c17e0c14cd13a573f75acd12
Red Fox <redfox.universe.25@gmail.com>
2025-12-06 17:16:27 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+"""
+Basic configuration file for My_Secure_Project.
+
+NOTE:
+In a real-world scenario, secrets like API keys
+should NEVER be hard-coded in source code.
+This file is intentionally misconfigured
+for OSINT / CTF training purposes.
+"""
+
+API_KEY = "CIU{very_security_repo}"  
+API_ENDPOINT = "https://api.ciu-redfox.example/v1"
+DEBUG_MODE = True
